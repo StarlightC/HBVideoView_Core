1b966d02acad2f8409a8aba98b7a6152933f352e
StarlightC <mail.starlightc@gmail.com>
2022-06-02 10:35:07 +0800

    update token

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -47,9 +47,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-
-    implementation("com.google.auto.service:auto-service:1.0")
-    kapt("com.google.auto.service:auto-service:1.0")
 }
 
 afterEvaluate {
@@ -68,7 +65,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.starlightc.video"
                 artifactId = "hbvideoview_core"
-                version = "0.0.7"
+                version = "0.0.9"
             }
         }
     }
