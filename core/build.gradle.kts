b745abba48a2d943fa8d0b1c2bfda1ce2a0f321f
StarlightC <mail.starlightc@gmail.com>
2022-06-06 18:39:18 +0800

    adjust core-ktx version

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.5.31")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
@@ -54,7 +54,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.starlightc"
                 artifactId = "hbvideoview_core"
-                version = "0.1.3"
+                version = "0.1.4"
             }
         }
     }
